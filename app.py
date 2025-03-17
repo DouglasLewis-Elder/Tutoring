8d1a644811cd224e1fa1927111c4792803e108d8
21086 <21086@wc.school.nz>
2025-03-18 12:30:03 +1300

    working in 5.5, sessions mostly work but login button still appears sometimes

--- app.py
+++ app.py
@@ -9,6 +9,14 @@ DATABASE = "tutor_db"
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 app.secret_key = "KartikIsASillyGoose"
+
+def is_logged_in():
+    if (session.get('account_id') is None):
+        print('Not logged in')
+        return False
+    else:
+        print("Loggedn in")
+        return True
 def connect_to_database(db_file):
     try:
         con = sqlite3.connect(db_file)
@@ -20,7 +28,7 @@ def connect_to_database(db_file):
 #ROUTES
 @app.route('/') #Home route
 def render_home():
-    return render_template('home.html')
+    return render_template('home.html', logged_in=is_logged_in())
 
 @app.route('/session_creation', methods=['POST','GET']) #Route for creating sessions
 def render_sessions():
@@ -99,10 +107,12 @@ def render_signup():
         connection.commit()
         connection.close()
         return render_template('login.html')
-    return render_template('signup.html')
+    return render_template('signup.html', logged_in=is_logged_in())
 
 @app.route('/login', methods=['POST','GET'])
 def render_login_page():
+    if is_logged_in():
+        return redirect('/session_list')
     #collect info from login page
     if request.method == 'POST':
         email = request.form['user_email'].strip().lower()
@@ -119,12 +129,30 @@ def render_login_page():
             account_id =  user_info[0]
             first_name = user_info[1]
             last_name = user_info[2]
+            type = user_info[3]
+            email = user_info[4]
+            user_password = user_info[5]
         except IndexError:
             return redirect('/login?error=email+or+password+invalid')
 
+        if not bcrypt.check_password_hash(user_password, password):
+            return redirect('/login?error=email+or+password+invalid')
+
+        session['email'] = email
+        session['account_id'] = account_id
+        session['first_name'] = first_name
+        print(session)
+        return redirect('/')
     #check info against DB info
     #save the info to a session
-    return render_template('login.html')
+    return render_template('login.html', logged_in=is_logged_in())
+
+@app.route('/logout', methods=['POST','GET'])
+def logout():
+    print(f'Session values before {session}')
+    session.clear()
+    print(f'Session values after {session}')
+    return redirect('/?message+See+you+next+time')
 
 if __name__ == '__main__':
     app.run()
